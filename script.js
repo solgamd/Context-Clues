8ddb589ee67d8cd55c137c857d935dec5da1702e
solgamd <solgamd@gmail.com>
2019-04-22 17:08:06 +0200

    Changed loop to attempt template literal

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 $(document).ready(function () {
-    for (i = 1; i < 101; i++) {
+    var i = 1;
 
-        $('body').append('<h3>' + 'Accusation ' + i + '</h3>');
-        if (i === 100) {
-            return;
-        }
+    while (i < 100) { // Appends all 100 Accusations onto document upon load
+        
+        //i++;
+        var h3 = ('<h3></h3>');
+        var h3text = `Accusation ${i}`;
+        $('h3').text(h3text);
+        $('body').append(h3);
+        
     };
-    $(h3).click(function () { // Not sure why this isn't working
-    console.log('worked');
-        alert('Accusation i: I accuse ' + homies + 'with the ' + implements + 'in ' + locales + '!')
+    
+
+    $('h3').click(function () {  // Randomly generate accusation upon click / DO NOT PUT THIS IN LOOP OR OUTSIDE DOC.READY
+        randomHomie = homies[Math.floor(Math.random() * homies.length)];
+        randomImplement = implements[Math.floor(Math.random() * implements.length)];
+        randomLocale = locales[Math.floor(Math.random() * locales.length)];
+
+        var i = 1;
+        if (i < 101) {
+            
+            alert(
+                `Accusation ${i}: I accuse ${randomHomie} with the ${randomImplement} in ${randomLocale}!`)
+            console.log('worked');
+            //i++;
+        }
+        
     })
+
+    var homies = ['Homer', 'Marge', 'Bart', 'Lisa', 'Maggie'];
+
+    var implements = ['false tooth', 'dull scissors', 'prison shank', 'fishing wire', 'spoon',
+        'rubber duck', 'paperback book', 'guitar pick', 'wooden plank', 'cucumber',
+        'pinecone', 'plastic knife', 'really really sharp oyster shell', 'object type thingy', 'microphone stand',
+        'soccer cleats', 'Persian rug', 'tambourine', 'horrible discography of John Mayer', 'photo of Snooki'];
+
+    var locales = ['the bathroom closet', 'the litter box',
+        'the pajama hamper', 'the moldy laundry room',
+        'the machete cabinet', 'the used underwear vending machine factory',
+        'Mr. Burns\'s wooden teeth cellar', 'your mom\'s lingerie drawer',
+        'the treehouse of horrors', 'that vague area in the nondescript place'];
+
+    var randomHomie;
+    var randomImplement;
+    var randomLocale;
+
 })
 
-// let homies = ['Homer', 'Marge', 'Bart', 'Lisa', 'Maggie'];
 
-// let locales = ['the bathroom closet', 'the cat food receptacle',
-//               'the pajama hamper', 'the moldy washing machine',
-//               'the machete cabinet', 'the used underwear vending machine',
-//               'Mr. Burns\'s wooden teeth preserver', 'your mom\'s lingerie drawer',
-//               'the treehouse of horrors', 'that vague area in the nondescript place'];
 
-// let implements = ['false tooth', 'dull scissors', 'prison shank', 'fishing wire', 'spoon',
-//                   'rubber duck', 'paperback book', 'guitar pick', 'wooden plank', 'cucumber',
-//                   'pinecone', 'plastic knife', 'really really sharp oyster shell', 'object type thingy', 'microphone stand',
-//                   'soccer cleats', 'Persian rug', 'tambourine', 'horrible discography of John Mayer', 'photo of Snooki'];
 
 
